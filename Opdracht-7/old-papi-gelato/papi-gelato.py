4dc0ca3c93f6d5cf6c6119f1b1445105f025c3ca
Matthijs4004 <matthijs4004@gmail.com>
2023-02-15 10:14:02 +0100

    converted python papi-gelato to javascript

---
+++ Opdracht-7/old-papi-gelato/papi-gelato.py
@@ -0,0 +1,172 @@
+# papi-gelato 2
+import math
+
+
+print("Welkom bij Papi Gelato.")
+sorry = "Sorry dat is geen optie die we aanbieden..."
+hoorntje = "Hoorntjes    1 x € 1.25    = € 1.25  "
+bakje = "Bakjes       1 x € 0.75    = € 0.75  "
+x = ("                          -------- +")
+
+def bon():
+    sprPrijs = hoeveel*0.30
+    bolPrijs = hoeveel*0.95
+    print("\n----------[Papi Gelato]----------\n")
+    print("Bolletjes   ",hoeveel,"x € 1.10    = €",round(bolPrijs, 2))
+    if topping == "Geen" and houder == "hoorntje":
+        print(hoorntje)
+        print(x)
+        print("Totaal                    = €",bolPrijs+1.25)
+    if topping == "Geen" and houder == "bakje":
+        print(bakje)
+        print(x)
+        print("Totaal                    = €",bolPrijs+0.75)
+    if topping == "Slagroom" and houder == "bakje":
+        print(bakje)
+        print("Topping      1 x € 0.50    = € 0.50  ")
+        print(x)
+        print("Totaal                     = €",bolPrijs+0.50+0.75)
+    elif topping == "Slagroom" and houder == "hoorntje":
+        print(hoorntje)
+        print("Topping      1 x € 0.50    = € 0.50  ")
+        print(x)
+        print("Totaal                     = €",bolPrijs+0.50+1.25)
+    if topping == "Sprinkels" and houder == "hoorntje":
+        print(hoorntje)
+        print("Topping      1 x €",round(sprPrijs, 2),"    = €",round(sprPrijs, 2))
+        print(x)
+        print("Totaal                     = €",bolPrijs+sprPrijs+1.25)
+    elif topping == "Sprinkels" and houder == "bakje":
+        print(bakje)
+        print("Topping      1 x €",round(sprPrijs, 2),"    = €",round(sprPrijs, 2))
+        print(x)
+        print("Totaal                     = €",bolPrijs+sprPrijs+0.75)
+    if topping == "Caramel" and houder == "hoorntje":
+        print(hoorntje)
+        print("Topping      1 x € 0.60  = € 0.60")
+        print(x)
+        print("Totaal                    = €",(bolPrijs+0.60+1.25))
+    elif topping == "Caramel" and houder == "bakje":
+        print(bakje)
+        print("Topping      1 x € 0.90    = € 0.90")
+        print(x)
+        print("Totaal                     = €",(bolPrijs+0.90+0.75))
+
+
+def zakelijkbon():
+    literPrijs = liter*9.80
+    quotient = literPrijs / 100
+    procent = quotient*6
+    
+    print("\n----------[Papi Gelato]----------\n")
+    print("Liter     ",liter," x € 9.80    = €",round(literPrijs, 2))
+    print(x)
+    print("Totaal                    = €",round(literPrijs,2))
+    print("BTW (6%)                  = €",round(procent, 2))
+
+def start():
+    print("\nBent u 1) particulier of 2) zakelijk?")
+    markt = int(input("> "))
+    if markt == 1:
+        stap1()
+    elif markt == 2:
+        zakelijk1()
+    else:
+        print(sorry)
+        return start()
+
+def zakelijk1():
+    global liter
+    print("\nHoeveel liter ijs wilt u?")
+    liter = int(input("> "))
+    if liter > 1:
+        zakelijk2()
+    else:
+        print(sorry)
+        return zakelijk1
+
+def zakelijk2():
+    for x in range(liter, 0, -1):
+        print("\nWelke smaak wilt u voor liter nummer",x,"? A) Aardbei, C) Chocolade of V) Vanille")
+        smaakL = input("> ")
+    if smaakL == "A" or smaakL == "C" or smaakL == "V":
+        zakelijkbon()
+    else:
+        print(sorry)
+        return zakelijk2()
+
+def stap1():
+    global hoeveel
+    print("\nHoeveel bolletjes wilt u?")
+    hoeveel = int(input("> "))
+    if hoeveel in range(1,4):
+        smaakKeuze()
+    elif hoeveel in range(4,8):
+        print("Dan krijgt u van mij een bakje met ",hoeveel," bolletjes")
+        smaakKeuze()
+    elif hoeveel > 8:
+        print("Sorry, zulke grote bakken hebben we niet")
+        return stap1()
+    else:
+        print(sorry)
+        return stap1()
+
+
+def smaakKeuze():
+    for x in range(hoeveel, 0, -1):
+        print("\nWelke smaak wilt u voor bolletje nummer",x,"? A) Aardbei, C) Chocolade of V) Vanille")
+        smaak = input("> ")
+    if smaak == "A" or smaak == "C"or smaak == "V":
+        stap2()
+    else:
+        print(sorry)
+        return smaakKeuze()
+
+
+def stap2():
+    global houder
+    print("\nWilt u deze",hoeveel,"bolletje(s) in A) een hoorntje of B) een bakje?")
+    houder = input("> ")
+    if houder == "A":
+        houder = "hoorntje"
+        toppings()
+    elif houder == "B":
+        houder = "bakje"
+        toppings()
+    else:
+        print(sorry)
+        return stap2()
+
+
+def toppings():
+    global topping
+    print("Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus?")
+    topping = input("> ")
+    if topping == "A":
+        topping == "Geen"
+        stap3()
+    elif topping == "B":
+        topping = "Slagroom"
+        stap3()
+    elif topping == "C":
+        topping = "Sprinkels"
+        stap3()
+    elif topping == "D":
+        topping = "Caramel"
+        stap3()
+    else:
+        print(sorry)
+
+
+def stap3():
+    print("\nHier is uw",houder,"met",hoeveel,"bolletje(s). Wilt u nog meer bestellen? Y/N")
+    nogmaals = input("> ")
+    if nogmaals == "Y":
+        stap1()
+    elif nogmaals == "N":
+        bon()
+        print("Bedankt en tot ziens!")
+    else:
+        print(sorry)
+
+start()
---
+++ Opdracht-7/script.js
@@ -0,0 +1,164 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+const pricesArray = {
+    'bolletje' : 1.10,
+    'bakje' : 0.75,
+    'hoorntje' : 1.25,
+    'slagroom' : 0.50, // extra kosten
+    'sprinkels' : 0.30, // per bolletje
+    'caramelB' : 0.90, // prijs bij een bakje
+    'caramelH' : 0.60 // prijs bij een hoorntje
+}
+const toppingArray = {
+    'sprinkels' : 0,
+    'caramel' : 0,
+    'slagroom' : 0,
+}
+const holderArray = {
+    'bakje' : 0,
+    'hoorntje' : 0
+}
+let bolletjesArray = {
+    'bolletjes' : 0
+}
+let bakje = false
+let hoorntje = false
+let flavor, holder, topping, again, bolletjesAantal
+
+// Execute first step
+stepOne()
+
+function stepOne() 
+{
+    bolletjesAantal = prompt( "Hoeveel bolletjes wilt u?" )
+    bolletjesArray[ 'bolletjes' ] += parseInt( bolletjesAantal )
+    if ( bolletjesAantal >= 1 && bolletjesAantal < 4) {
+        flavorChoice(  )
+    } else if ( bolletjesAantal >= 4 && bolletjesAantal <= 8 ) {
+        holderArray[ 'bakje' ] += 1
+        bakje = true
+        flavorChoice()
+    } else if ( bolletjesAantal > 8 ) {
+        alert( "Sorry, zulke grote bakken hebben we niet." )
+        stepOne()
+    } else {
+        alert( "Het programma kan dit antwoord niet doorvoeren, probeer opnieuw" )
+        stepOne()
+    }
+}
+
+function flavorChoice() 
+{
+    for ( let i = 1; i < parseInt(bolletjesAantal) + 1; i++) {
+        flavor = prompt( "Welke smaak wilt u voor bolletje nummer " + i + "? A) Aardbei, C) Chocolade of V) Vanille" )
+    }
+    if ( flavor.toLowerCase() === "a" || flavor.toLowerCase() === "c" || flavor.toLowerCase() === "v") {
+        holderChoice()
+    } else {
+        alert( "Het programma kan dit antwoord niet doorvoeren, probeer opnieuw" )
+        flavorChoice()
+    }
+    
+}
+
+function holderChoice() 
+{
+    if ( !holderArray['bakje'] ) {
+        holder = prompt( "Wilt u deze " + bolletjesAantal + " bolletje(s) in A) een hoorntje of B) een bakje?" )
+
+        if ( holder.toLowerCase() === "a" || holder.toLowerCase() === "hoorntje" ) {
+            holderArray[ 'hoorntje' ] += 1
+            hoorntje = true
+            toppings()
+        } else if ( holder.toLowerCase() === "b" || holder.toLowerCase() === "bakje" ) {
+            holderArray[ 'bakje' ] += 1
+            bakje = true
+            toppings()
+        } else {
+            alert( "Het programma kan dit antwoord niet doorvoeren, probeer opnieuw" )
+            holderChoice()
+        }
+
+    } else {
+        toppings()
+    }
+}
+
+function toppings()
+{
+    topping = prompt( "Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus?" )
+    switch ( topping.toLowerCase() ) {
+        case "a":
+            orderMore()
+            break
+        case "b":
+            toppingArray[ 'slagroom' ] += 1
+            orderMore()
+            break
+        case "c":
+            toppingArray[ 'sprinkels' ] += bolletjesAantal
+            orderMore()
+            break
+        case "d":
+            toppingArray[ 'caramel' ] += 1
+            orderMore()
+            break
+    }
+}
+
+function orderMore() 
+{
+    if ( holderArray[ 'bakje' ] ) {
+        again = prompt( "Hier is uw bakje met " + bolletjesAantal + " bolletje(s). Wilt u nog meer bestellen? Y/N" )
+    } else {
+        again = prompt( "Hier is uw hoorntje met " + bolletjesAantal + " bolletje(s). Wilt u nog meer bestellen? Y/N" )
+    }
+
+    if ( again.toLowerCase() === "y" ) {
+        stepOne()
+    } else if ( again.toLowerCase() === "n" ) {
+        receipt()
+    } else {
+        alert( "Het programma kan dit antwoord niet doorvoeren, probeer opnieuw" )
+    }
+}
+
+function receipt() 
+{
+    let body = document.querySelector( 'body' )
+    let sprinkelsPrice = bolletjesArray[ 'bolletjes' ] * pricesArray[ 'sprinkels' ]
+    let bolletjesPrice = parseFloat( ( bolletjesArray[ 'bolletjes' ] * pricesArray[ 'bolletje' ] ).toFixed(2) )
+    let totalPrice = parseFloat(bolletjesPrice) + parseFloat( holderArray[ 'bakje' ] * pricesArray[ 'bakje' ] ) + parseFloat( holderArray[ 'hoorntje' ] * pricesArray[ 'hoorntje' ] ) + parseFloat( toppingArray[ 'slagroom' ] * pricesArray[ 'slagroom' ] )
+
+    body.innerHTML += "<h2>-------Papi Gelato-------</h2>"
+    body.innerHTML += "<p>Bolletjes " + bolletjesArray[ 'bolletjes' ] + " x € " + pricesArray[ 'bolletje' ].toFixed(2) + " = €" + bolletjesPrice.toFixed(2) + "</p>"
+    if ( holderArray[ 'bakje' ] > 0 ) {
+        body.innerHTML += "<p>Bakje " + holderArray[ 'bakje' ] + " x €" + pricesArray[ 'bakje' ].toFixed(2) + " = €" + ( holderArray[ 'bakje' ] * pricesArray[ 'bakje' ]).toFixed(2)  + "</p>"
+    }
+    if ( holderArray[ 'hoorntje' ] > 0 ) {
+        body.innerHTML += "<p>Hoorntje " + holderArray[ 'hoorntje' ] + " x €" + pricesArray[ 'hoorntje' ].toFixed(2) + " = €" + ( holderArray[ 'hoorntje' ] * pricesArray[ 'hoorntje' ]).toFixed(2)  + "</p>"
+    }
+    if ( toppingArray[ 'sprinkels' ] !== 0 ) {
+        totalPrice += parseFloat( sprinkelsPrice )
+        body.innerHTML += "<p>Sprinkels " + bolletjesArray[ 'bolletjes' ] + " x €" + pricesArray[ 'sprinkels' ].toFixed(2) + " = €" + ( sprinkelsPrice ).toFixed(2)  + "</p>"
+    }
+    if ( toppingArray[ 'slagroom' ] !== 0 ) {
+        body.innerHTML += "<p>Slagroom " + toppingArray[ 'slagroom' ] + " x €" + pricesArray[ 'slagroom' ].toFixed(2) + " = €" + ( toppingArray[ 'slagroom' ] * pricesArray[ 'slagroom' ] ).toFixed(2)  + "</p>"
+    } 
+    if ( toppingArray[ 'caramel' ] !== 0 && holderArray[ 'bakje' ] !== 0 ) {
+        totalPrice += parseFloat( toppingArray[ 'caramel' ] * pricesArray[ 'caramelB' ] )
+        body.innerHTML += "<p>Caramel (B) " + toppingArray[ 'caramel' ] + " x €" + pricesArray[ 'caramelB' ].toFixed(2) + " = €" + ( toppingArray[ 'caramel' ] * pricesArray[ 'caramelB' ] ).toFixed(2)  + "</p>"
+    }
+    if ( toppingArray[ 'caramel' ] !== 0 && holderArray[ 'hoorntje' ] !== 0 ) {
+        totalPrice += parseFloat( toppingArray[ 'caramel' ] * pricesArray[ 'caramelH' ])
+        body.innerHTML += "<p>Caramel (H) " + toppingArray[ 'caramel' ] + " x €" + pricesArray[ 'caramelH' ].toFixed(2) + " = €" + ( toppingArray[ 'caramel' ] * pricesArray[ 'caramelH' ] ).toFixed(2)  + "</p>"
+    }
+    console.log( holderArray )
+    console.log( pricesArray )
+    console.log( bolletjesArray )
+    console.log( toppingArray )
+
+    body.innerHTML += "<p>Totaal: €" + totalPrice.toFixed(2) + "</p>"
+}
+
+});
